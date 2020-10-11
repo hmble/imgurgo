0912aa4b8213983f149949f88b3acc89d3b5aa2f
Samyak Bakliwal <w3bcode@gmail.com>
2020-10-11 15:58:43 +0530

    fix docs

--- album.go
+++ album.go
@@ -22,7 +22,6 @@ func (a *AlbumService) Info(id string) {
 
 // Return all of the images in the album
 // Model Reference: https://api.imgur.com/models/image
-
 func (a *AlbumService) AllImages(id string) {
 	path := fmt.Sprintf("/album/%s/images", id)
 
@@ -39,7 +38,6 @@ func (a *AlbumService) AllImages(id string) {
 
 // Get information about an image in an album, any additional actions found in
 // Image Endpoint will also work.
-
 func (a *AlbumService) Image(albumId, imageId string) {
 	path := fmt.Sprintf("/album/%s/image/%s", albumId, imageId)
 
@@ -54,21 +52,8 @@ func (a *AlbumService) Image(albumId, imageId string) {
 
 }
 
-// Create a new album. Optional parameter of ids[] is an array of image ids to add
-// to the album.
-
-// array of image ids to add to the album. If uploading anonymous images to
-// anonymous album please use the optional parameter of deletehashes[] rather than
-// ids[]. Note: including the optional deletehashes[] parameter will also work for
-// authenticated user albums. There is no need to duplicate image ids with their
-// corresponding deletehash.
-
-// This method is available without authenticating an account, and may be used
-// merely by sending "Authorization: Client-ID {client_id}" in the request headers.
-// Doing so will create an anonymous album which is not tied to an account.
-
 // TODO(hmble): Add support for anonymous image upload.
-
+// TODO(hmble): More better way to create postdata
 // Key                                  Description
 // ================================================================================
 // ids[]             ( optional ) The image ids that you want to be included in the album.
@@ -80,7 +65,16 @@ func (a *AlbumService) Image(albumId, imageId string) {
 // layout            ( optional ) Sets the layout to display the album. Values are : blog | grid | horizontal | vertical
 // cover             ( optional ) The ID of an image that you want to be the cover of the album
 
-// TODO(hmble): More better way to create postdata
+// Create a new album. Optional parameter of ids[] is an array of image ids to add
+// to the album.
+// array of image ids to add to the album. If uploading anonymous images to
+// anonymous album please use the optional parameter of deletehashes[] rather than
+// ids[]. Note: including the optional deletehashes[] parameter will also work for
+// authenticated user albums. There is no need to duplicate image ids with their
+// corresponding deletehash.
+// This method is available without authenticating an account, and may be used
+// merely by sending "Authorization: Client-ID {client_id}" in the request headers.
+// Doing so will create an anonymous album which is not tied to an account.
 func (a *AlbumService) CrateAlbum(postdata PostData) {
 	path := "/album"
 
@@ -95,13 +89,6 @@ func (a *AlbumService) CrateAlbum(postdata PostData) {
 	printBytes(resp.Body, a.client)
 }
 
-// Update the information of an album. For anonymous albums, {album} should be the
-// deletehash that is returned at creation. If uploading anonymous images to
-// anonymous album please use the optional parameter of deletehashes[] rather than
-// ids[]. Note: including the optional deletehashes[] parameter will also work for
-// authenticated user albums. There is no need to duplicate image ids with their
-// corresponding deletehash.
-//
 // Key                                  Description
 // ================================================================================
 // ids[]             ( optional ) The image ids that you want to be included in the album.
@@ -113,6 +100,12 @@ func (a *AlbumService) CrateAlbum(postdata PostData) {
 // layout            ( optional ) Sets the layout to display the album. Values are : blog | grid | horizontal | vertical
 // cover             ( optional ) The ID of an image that you want to be the cover of the album
 
+// Update the information of an album. For anonymous albums, {album} should be the
+// deletehash that is returned at creation. If uploading anonymous images to
+// anonymous album please use the optional parameter of deletehashes[] rather than
+// ids[]. Note: including the optional deletehashes[] parameter will also work for
+// authenticated user albums. There is no need to duplicate image ids with their
+// corresponding deletehash.
 func (a *AlbumService) UpdateAlbum(deletehash string, postdata PostData) {
 	path := fmt.Sprintf("/album/%s", deletehash)
 
@@ -130,7 +123,6 @@ func (a *AlbumService) UpdateAlbum(deletehash string, postdata PostData) {
 // Delete an album with a given ID. You are required to be logged in as the user to
 // delete the album. For anonymous albums, {album} should be the deletehash that is
 // returned at creation.
-
 func (a *AlbumService) DeleteAlbum(deletehash string) {
 	path := fmt.Sprintf("/album/%s", deletehash)
 
@@ -147,7 +139,6 @@ func (a *AlbumService) DeleteAlbum(deletehash string) {
 
 // Favorite an album with a given ID. The user is required to be logged in to
 // favorite the album.
-
 func (a *AlbumService) FavoriteAlbum(albumid string) {
 	path := fmt.Sprintf("/album/%s/favorite", albumid)
 
@@ -162,13 +153,6 @@ func (a *AlbumService) FavoriteAlbum(albumid string) {
 	printBytes(resp.Body, a.client)
 }
 
-// Sets the images for an album, removes all other images and only uses the
-// images in this request. For anonymous albums, {album} should be the
-// deletehash that is returned at creation. If uploading anonymous images to
-// anonymous album please use the optional parameter of deletehashes[] rather
-// than ids[]. Note: including the optional deletehashes[] parameter will also
-// work for authenticated user albums. There is no need to duplicate image ids
-// with their corresponding deletehash.
 //
 // Key                              Description
 //======================================================================
@@ -177,6 +161,13 @@ func (a *AlbumService) FavoriteAlbum(albumid string) {
 // deletehashes[]    ( optional ) The image deletehashes that you want to be added to the album.
 //                            The [] represents the ability to use this variable as an array.
 
+// Sets the images for an album, removes all other images and only uses the
+// images in this request. For anonymous albums, {album} should be the
+// deletehash that is returned at creation. If uploading anonymous images to
+// anonymous album please use the optional parameter of deletehashes[] rather
+// than ids[]. Note: including the optional deletehashes[] parameter will also
+// work for authenticated user albums. There is no need to duplicate image ids
+// with their corresponding deletehash.
 func (a *AlbumService) SetAlbumImages(deletehash string, postdata PostData) {
 	path := fmt.Sprintf("/album/%s", deletehash)
 
@@ -191,12 +182,6 @@ func (a *AlbumService) SetAlbumImages(deletehash string, postdata PostData) {
 	printBytes(resp.Body, a.client)
 }
 
-// For anonymous albums, {album} should be the deletehash that is returned at
-// creation. If uploading anonymous images to anonymous album please use the
-// optional parameter of deletehashes[] rather than ids[]. Note: including the
-// optional deletehashes[] parameter will also work for authenticated user
-// albums. There is no need to duplicate image ids with their corresponding
-// deletehash.
 //
 // Key                              Description
 //======================================================================
@@ -204,6 +189,16 @@ func (a *AlbumService) SetAlbumImages(deletehash string, postdata PostData) {
 //                                The [] represents the ability to use this variable as an array.
 // deletehashes[]    ( optional ) The image deletehashes that you want to be added to the album.
 //                            The [] represents the ability to use this variable as an array.
+
+// NOTE : we can only add one id at a time
+// Reference: https://apidocs.imgur.com/#b98029b6-5cc1-4a6f-b4bf-fe1db50869a2
+
+// For anonymous albums, {album} should be the deletehash that is returned at
+// creation. If uploading anonymous images to anonymous album please use the
+// optional parameter of deletehashes[] rather than ids[]. Note: including the
+// optional deletehashes[] parameter will also work for authenticated user
+// albums. There is no need to duplicate image ids with their corresponding
+// deletehash.
 func (a *AlbumService) AddImagesToAlbum(deletehash string, postdata PostData) {
 	path := fmt.Sprintf("/album/%s/add", deletehash)
 
@@ -218,15 +213,15 @@ func (a *AlbumService) AddImagesToAlbum(deletehash string, postdata PostData) {
 	printBytes(resp.Body, a.client)
 }
 
-// Takes parameter, ids[], as an array of ids and removes from the album. For
-// anonymous albums, {album} should be the deletehash that is returned at
-// creation.
 //
 // Key                              Description
 //======================================================================
 // ids[]             ( required ) The image ids that you want to be removed from the album.
 //                                The [] represents the ability to use this variable as an array.
 
+// Takes parameter, ids[], as an array of ids and removes from the album. For
+// anonymous albums, {album} should be the deletehash that is returned at
+// creation.
 func (a *AlbumService) RemoveImages(deletehash string, opts Option) {
 	path := fmt.Sprintf("/album/%s/remove_images", deletehash)
 
